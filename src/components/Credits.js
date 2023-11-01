9d6406d7ce214eddaa467ffac32d2dd23039f605
Rosa Rivera <riverasoto.rosa@gmail.com>
2023-11-01 15:43:57 -0400

    Update Credits.js
    added the functionality for credit

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,11 +5,20 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import AccountBalance from './AccountBalance';
+
+const Credits = (props) => {
+  const { credits, balance, updateBalance, updateCreditList } = props;
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState('');
+
 
 const Credits = (props) => {
   return (
     <div>
       <h1>Credits</h1>
+    <ul style={{listStyle: 'none', paddingLeft: 0, textalign: 'center'}}>
+    
       <br/>
       <Link to="/">Return to Home</Link>
     </div>
