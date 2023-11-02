45cc5e1a073114aaca4b93d801fbcaa3ddf70182
Eliana Torres <elianatorres3114@gmail.com>
2023-11-01 23:39:38 -0400

    t

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -35,35 +35,33 @@ const Debits = (props) => {
   }
   // Render the list of Debit items and a form to input new Debit item
   return (
-    <div style = {{textAlign: 'center'}}>
+    <div style={{textAlign: 'center'}}>
       <h1>Debits</h1>
       <ul style={{listStyle: 'none', paddingLeft: 0, textalign: 'center'}}>
-
-      {
-        Debits.map((debit) => (
+        {debits.map((debit) => (
         <li key={debit.id}>
           <div className='debits'>
             <p>Description: {debit.description}</p>
-            <p>Amount: ${parseFloat(debit.amount).toFixed(2)}</p>
-            <p>Date: {new Date(debit.date).toISOString().split('T')[0]}</p>
+            <p>Amount:${parseFloat(debit.amount).toFixed(2)}</p>
+            <p>Date:{new Date(debit.date).toISOString().split('T')[0]}</p>
           </div>
         </li>
-      ))}
-
+        ))}
       </ul>
       <br/>
-        <AccountBalance accountBalance = {balance}/>
-      <br/> 
-      <form onSubmit={props.addDebit}>
-        <input type="text" name="description" onChange={re => setDescription(re.target.value)}/>
-        <input type="number" name="amount" onChange={re => setAmt(re.target.value)} />
-        <button type="submit">Add Debit</button>
-      </form>
-      <br/>
+      <AccountBalance accountBalance={balance}/>
+      <div className='debit-addition'>
+        <h3>Debit</h3>
+        <label htmlFor='description'>Description:</label>
+        <input type="text" placeholder="Description" id="description" onChange={re => setDescription(re.target.value)}/>
+        <label htmlFor='amount'>Amount:</label>
+        <input type="number" placeholder="Amount" id="amount" onChange={re => setAmt(re.target.value)}/>
+        <button type='submit'>Add Debit</button>
+      </div>
 
+      <br/>
       <Link to="/">Return to Home</Link>
     </div>
   );
 }
-
 export default Debits;
